404dcb5cbfd19c52a153031118001dd1d1394328
Chester John <chesterjohn@Chesters-MBP.lan>
2015-09-16 16:54:10 +0800

    authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
 	has_many :microposts
-	validates :name, presence: true
-	validates :email, presence: true
+	has_secure_password
+	validates :password, length: {minimum: 6}
+	before_save { self.email = email.downcase }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :name, presence: true, length: {maximum: 50}
+	validates :email, presence: true, length: {maximum: 255},
+	                  format: { with: VALID_EMAIL_REGEX },
+	                  uniqueness: { case_sensitive: false }
+
 end
