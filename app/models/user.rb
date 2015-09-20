5c6550ee1853650dc9424f36a381fbc19d05cd41
Chester John <chesterjohn@Chesters-MBP.lan>
2015-09-20 15:54:02 +0800

    use ssl and puma web server in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
 	has_many :microposts
 	has_secure_password
-	validates :password, length: {minimum: 6}
 	before_save { self.email = email.downcase }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, presence: true, length: {maximum: 50}
 	validates :email, presence: true, length: {maximum: 255},
 	                  format: { with: VALID_EMAIL_REGEX },
 	                  uniqueness: { case_sensitive: false }
+	validates :password, length: { minimum: 6 }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-
+  root 'static_pages#home'
   get 'help' => 'static_pages#help'
-
   get 'about' => 'static_pages#about'
-
   get 'contact' => 'static_pages#contact'
-
   get 'signup' => 'users#new'
 
   resources :microposts
@@ -15,7 +12,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'static_pages#home'
+ 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
