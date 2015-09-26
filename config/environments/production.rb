37e4f72204bb0c63ddc8296d67632cfd483dca4d
Chester John <chesterjohn@Chesters-MacBook-Pro.local>
2015-09-26 16:50:11 +0800

    user activation,password reset, and email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = "glacial-escarpment-4608.herokuapp.com"
-  confi.action_mailer.default_url_options = {host: host}
+  config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
